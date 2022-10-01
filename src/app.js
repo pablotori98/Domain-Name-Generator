6246ced8f1aa0bc714dea00298c95f96111022a6
pablotori98 <pablotori98@gmail.com>
2022-10-01 11:29:32 +0000

    primera entrega pablo

--- src/app.js
+++ src/app.js
@@ -8,4 +8,20 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var dom = ["com", "net"];
+
+  let domain;
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < dom.length; d++) {
+          domain = pronoun[i] + adj[a] + noun[n] + "." + dom[d];
+          console.log(domain);
+        }
+      }
+    }
+  }
 };
